97e3b8b904ad12218ea97e95cf5a0430bc82421e
Jon Sundin <sundinjon@gmail.com>
2024-10-29 16:56:10 -0700

    added force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
-import Link from "next/link";
+import { v4 as uuidv4 } from 'uuid';
+import { db } from "~/server/db";
+
+export const dynamic = "force-dynamic";
 
 const mockUrls = [
   "https://utfs.io/f/tfG8JEUc2NGuBKLWBEQIYdcGx5hJvj8Ki9qZM4PmSX3RBogF",
@@ -8,16 +11,24 @@ const mockUrls = [
 ];
 
 const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
+  id: uuidv4(),
   url,
 }))
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h2>{post.name}</h2>
+          </div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image, i) => (
+          <div key={image.id + i} className="w-48">
             <img src={image.url} alt={`Image ${image.id}`} className="w-full h-auto" />
           </div>
         ))}
